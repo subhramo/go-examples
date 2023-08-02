b1d8089f4684ffa428e7ceb2d7a9b8e86ed98602
Subhra Mondal <subhra.mondal16@yahoo.com>
2023-08-02 14:10:00 +1000

    New code

---
+++ src/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	// "github.com/google/go-github/v52/github"
+	// "golang.org/x/oauth2"
+)
+
+func main() {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: "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"},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// Define pull request parameters
+	newPR := &github.NewPullRequest{
+		Title:               github.String("Pull Request Title"),
+		Head:                github.String("BranchName"), // e.g., "feature-branch"
+		Base:                github.String("main"),       // Typically "main" or "master"
+		Body:                github.String("Pull request description."),
+		MaintainerCanModify: github.Bool(true),
+	}
+
+	// Create the pull request
+	pr, _, err := client.PullRequests.Create(ctx, "<Owner>", "<Repo>", newPR)
+	if err != nil {
+		fmt.Printf("Error while creating the pull request: %v\n", err)
+		return
+	}
+
+	// Print the created pull request
+	fmt.Printf("Created PR: %v\n", pr)
+}
